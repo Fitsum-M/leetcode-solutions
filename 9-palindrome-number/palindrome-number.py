75a9f05527f734314f458c65fe3a39b5c5d57332
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-05 06:05:05 -0800

    Time: 8 ms (69.09%) | Memory: 12.5 MB (52.03%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if(x<0):
+          return False;
+        rev=0
+        original=x
+        while(x!=0):
+            r=x%10
+            rev=rev*10+r
+            x=x//10
+        
+        return rev==original
+        
