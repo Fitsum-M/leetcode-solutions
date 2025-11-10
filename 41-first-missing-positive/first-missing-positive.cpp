9270d9dedaac0c7c8c26331274c7df7fda3fa7ad
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-10 08:00:25 -0800

    Time: 135 ms (5.07%) | Memory: 80 MB (5.37%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n=nums.size();
+        map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]];
+        }
+        for(int i=1;i<=n;i++){
+            if(mpp.find(i)==mpp.end()) return i;
+            
+        }
+        return n+1;
+    }
+};
