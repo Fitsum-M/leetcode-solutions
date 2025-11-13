12955fa0099d8e6b8984bc20a48fde265bfc7bd3
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-13 03:49:34 -0800

    Time: 11 ms (5.53%) | Memory: 13.9 MB (79.70%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+       int n=nums1.size();
+       int m=nums2.size();
+       
+       vector<int>che;
+       set<int>srs;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(nums1[i]==nums2[j]){
+                 srs.insert(nums1[i]);    
+            }
+        }
+       }
+       for(auto s:srs){
+            che.push_back(s);
+        }
+        return che;
+      
+    }
+};
