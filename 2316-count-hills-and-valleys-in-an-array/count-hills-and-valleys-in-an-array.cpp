582c78ad75826eb920aad1a19313024bcddda755
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-14 05:18:49 -0800

    Time: 0 ms (100.00%) | Memory: 12.3 MB (15.63%) - LeetSync

---
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+       vector<int>arr;
+       int count=0;
+       arr.push_back(nums[0]);
+       for(int i=1;i<nums.size();i++){
+        if(nums[i]!=nums[i-1]){
+            arr.push_back(nums[i]);
+        }
+
+       }
+       int n=arr.size();
+       for(int i=1;i<n-1;i++){
+           if (arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                   count++;
+           }    
+    else if (arr[i] < arr[i-1] && arr[i] < arr[i+1]){
+           count++;
+    }  
+       }
+   return count;
+    }
+};
