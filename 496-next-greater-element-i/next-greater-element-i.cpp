83a303cd98aa5c637670c9c1c32b1604ae6b03c7
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-15 03:28:50 -0800

    Time: 3 ms (41.75%) | Memory: 13 MB (14.15%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,27 +3,34 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size();
         int m=nums2.size();
-        vector<int>chr;
+        if(m == 0) return vector<int>(n, -1);
+        stack<int>stk;
+        vector<int>v(m,-1);
+        if(nums2.size()!=0)v[m-1]=-1;
+        stk.push(nums2[m-1]);
+        for(int i=m-1;i>=0;i--){
+             while(!stk.empty()  && stk.top() <=nums2[i]){
+                stk.pop();
+             }
+             if(stk.empty()){
+                 v[i]=-1; 
+             }
+             else {
+                v[i]=stk.top();
+             }
+             stk.push(nums2[i]);
+        }
+        map<int,int>mapp;
+        vector<int>ans(n);
+        for(int i=0;i<m;i++){
+            mapp[nums2[i]]=i;
+        }
         for(int i=0;i<n;i++){
-            bool found = false;
-            for(int j=0;j<m;j++){
-                  if(nums1[i]==nums2[j]){
-                    for(int k=j+1;k<m;k++){
-                        if(nums2[j]<nums2[k]){
-                             chr.push_back(nums2[k]);
-                              found=true;
-                             break;
-                        }
-                    
-                  }
-                  }
-            }
-
-            if(!found){
-                chr.push_back(-1);
+            if(mapp.find(nums1[i])!=mapp.end()){
+                int index=mapp[nums1[i]];
+                ans[i]=v[index];
             }
         }
-        return chr;
-        
+        return ans;
     }
 };
