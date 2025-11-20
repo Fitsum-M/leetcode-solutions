303a6cbb6289f2a6d415e58b0d28e90f526fd542
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-19 22:55:16 -0800

    Time: 0 ms (100.00%) | Memory: 13.8 MB (99.87%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int n=logs.size();
+        int depth=0;
+        for(int i=0;i<n;i++){
+            if(logs[i]=="../"){
+                if(depth>0) depth--;
+            }
+            else if(logs[i]=="./"){
+                continue;
+            }
+            else{
+                depth++;
+            }
+        }
+        return depth;
+    }
+};
