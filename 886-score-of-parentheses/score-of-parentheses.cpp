6b7016570430e40d6ff8ac48af04407d153b9790
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-20 04:39:56 -0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (18.98%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.cpp
+++ 886-score-of-parentheses/score-of-parentheses.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
-    int scoreOfParentheses(std::string s) {
-        std::stack<int> score_stack;
-        score_stack.push(0); 
-
-        for (char c : s) {
-            if (c == '(') {
-                score_stack.push(0);
-            } else { 
-                int inner_score = score_stack.top();
-                score_stack.pop();
-
-                int outer_score = score_stack.top();
-                score_stack.pop();
-
-                int current_group_score = std::max(1, 2 * inner_score);
-                
-                score_stack.push(outer_score + current_group_score);
+    int scoreOfParentheses(string s) {
+        int n=s.size();
+        int count=0;
+        stack<int>stk;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(') stk.push(-1);
+            else if(s[i]==')') {
+                int sum=0;
+                while(stk.top()!=-1){
+                    sum+=stk.top();
+                    stk.pop();
+                }
+                if(sum==0){
+                    stk.pop();
+                    stk.push(1);
+                }
+                else{
+                    stk.pop();
+                   sum*=2;
+                   stk.push(sum); 
+                }
             }
         }
+        while(!stk.empty()){
+            count+=stk.top();
+            stk.pop();
+        }
+        return count;
 
-        return score_stack.top();
     }
 };
