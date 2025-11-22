6cb64ce23f6dd4041d852c3513f54997c24fc712
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-22 11:14:50 -0800

    Time: 0 ms (100.00%) | Memory: 12 MB (79.28%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int countHillValley(vector<int>& nums) {
+    int countHillValley(vector<int>& arr1) {
+       int n=arr1.size();
        vector<int>arr;
+       arr.push_back(arr1[0]);
+       for(int i=1;i<n;i++){
+          if(arr1[i]!=arr1[i-1]){
+            arr.push_back(arr1[i]);
+          }
+       }
        int count=0;
-       arr.push_back(nums[0]);
-       for(int i=1;i<nums.size();i++){
-        if(nums[i]!=nums[i-1]){
-            arr.push_back(nums[i]);
+       for(int i=1;i<arr.size()-1;i++){
+        if(arr[i]<arr[i-1] && arr[i]<arr[i+1]){
+            count ++;
         }
-
+       if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+            count ++;
+        }
        }
-       int n=arr.size();
-       for(int i=1;i<n-1;i++){
-           if (arr[i] > arr[i-1] && arr[i] > arr[i+1]){
-                   count++;
-           }    
-    else if (arr[i] < arr[i-1] && arr[i] < arr[i+1]){
-           count++;
-    }  
-       }
-   return count;
+       return count;
+    
     }
 };
