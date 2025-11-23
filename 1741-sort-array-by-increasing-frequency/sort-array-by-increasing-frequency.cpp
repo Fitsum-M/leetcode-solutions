085293647355438a01ba97bad83e2fcc579d2747
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-23 11:26:39 -0800

    Time: 7 ms (11.96%) | Memory: 16.4 MB (5.52%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        unordered_map<int,int>freq;
+        int n=nums.size();
+        map<int,int>freq;
         for(int x:nums){
-            freq[x]++;
+             freq[x]++;
         }
-        sort(nums.begin(),nums.end(),[&](int a,int b){
-        if(freq[a]==freq[b]){
-            return a>b;
-        }
-        return freq[a]<freq[b];
+        sort(nums.begin(),nums.end() ,[&](int a,int b){
+            if(freq[a]==freq[b]){
+                return a>b;
+            }
+            return freq[a]<freq[b];
         });
         return nums;
     }
-    
-    };
+};
