71dbce2f338760cf9c0405e1155a6b51900f3469
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-23 10:46:48 -0800

    Time: 0 ms (100.00%) | Memory: 21.8 MB (37.41%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-      int n=nums.size();
-      int sum=n*(n+1)/2;
-
-      int arrsum=0;
-      for(int x:nums) arrsum+=x;
-      return sum-arrsum;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i!=nums[i]) return i;
+        }
+        return n;
     }
 };
