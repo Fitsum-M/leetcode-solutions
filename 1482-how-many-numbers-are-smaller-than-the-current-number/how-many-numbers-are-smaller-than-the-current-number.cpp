4c99132eee4146e04a6bdf71e9876fa87e5d3f7f
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-23 11:20:37 -0800

    Time: 15 ms (11.56%) | Memory: 14.2 MB (53.69%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-      int n=nums.size();
-      vector<int>hash(101,0);
-      for(int i=0;i<n;i++){
-        hash[nums[i]]++;
-      }
-      vector<int>v(101,0);
-      v[0]=hash[0];
-      for(int i=1;i<101;i++){
-           v[i]=v[i-1]+hash[i];
-      }
-      vector<int>ans(n);
-      for(int i=0;i<n;i++){
-        if(nums[i]!=0) ans[i]=v[nums[i]-1];
-        else ans[i]=0;
-      }
-        return ans;
+        int n=nums.size();
+        vector<int>v;
+        
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(i!=j && nums[i]>nums[j]){
+                    count++;
+                }
+            }
+             v.push_back(count);
+        }
+        return v;
     }
 };
