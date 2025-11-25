719b7cd0334eac33a8ae43d2632703f6540547b1
Fitsum-M <fitsummulugeta22@gmail.com>
2025-11-25 05:05:05 -0800

    Time: 117 ms (5.68%) | Memory: 95 MB (6.30%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int>mp;
+        for(int x:nums){
+            mp[x]++;
+        }
+        for(auto &p:mp){
+            if(p.second>=2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
